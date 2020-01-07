043840fed9be254b2cfb4f763bbcda0a1dfc9ab9
Trenton Weller <trenton.weller1298@gmail.com>
2020-01-07 13:59:41 -0500

    Added Const.

--- sketch_jan06a/sketch_jan06a.ino
+++ sketch_jan06a/sketch_jan06a.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ *Progammer: Trent
+ *Date: 1.7.20
+*/
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+const int digitPins[4] = {
+  4,5,6,7};                 //4 common anode pins of the display
+const int clockPin = 11;    //74HC595 Pin 11 
+const int latchPin = 12;    //74HC595 Pin 12
+const int dataPin = 13;     //74HC595 Pin 14
+const int tempPin = A0;     //temperature sensor pin
+const byte digit[10] =      //seven segment digits in bits
+{
